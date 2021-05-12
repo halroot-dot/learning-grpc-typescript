bb1b4f035cd93f7b0b92dbaa8d155746e136c9ba
halroot-dot <auayu620@gmail.com>
2021-05-12 18:08:49 +0900

    Update test.sh

--- example5/test.sh
+++ example5/test.sh
@@ -2,7 +2,9 @@
 
 set -eu
 
-export PATH="$PATH:$(npm bin)"
+NPM_BI=$(npm bin)
+PLUGIN_GRPC=$(npm bin)/grpc_tools_node_protoc_plugin
+PLUGIN_TS=$(npm bin)/protoc-gen-ts
 
 PROTO_SRC=./proto
 PROTO_DEST=./src/proto
@@ -10,14 +12,10 @@ PROTO_DEST=./src/proto
 mkdir -p ${PROTO_DEST}
 
 grpc_tools_node_protoc \
-  --plugin=protoc-gen-grpc=$(which grpc_tools_node_protoc_plugin) \
   --js_out=import_style=commonjs,binary:${PROTO_DEST} \
-  --grpc_out=grpc_js:${PROTO_DEST} \
-  -I ${PROTO_SRC} \
-  ${PROTO_SRC}/*
-
-grpc_tools_node_protoc \
-  --plugin=protoc-gen-ts=$(npm bin)/protoc-gen-ts \
   --ts_out=grpc_js:${PROTO_DEST} \
+  --grpc_out=grpc_js:${PROTO_DEST} \
+  --plugin=protoc-gen-grpc=${PLUGIN_GRPC} \
+  --plugin=protoc-gen-ts=${PLUGIN_TS} \
   -I ${PROTO_SRC} \
   ${PROTO_SRC}/*
